22181c92c0b27888fda4ab497785058de35a8b9c
lintatreasa <lintatreasa@gmail.com>
2016-11-01 18:21:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+	<title>HoverBoard</title>
+	<link rel="stylesheet" type="text/css" href="styles/global.css"/>
+</head>
+<body> 
+ <div id="header">
+ <div class="logo"><a href="#">Hover<span>Board</span></a> </div>
+    <div class="login"><a href="login.html">Login&nbsp;&nbsp;</a> <a href="signup.php"> Signup</a></div>
+ </div>
+ <div id="container">
+ <div class="sidebar">
+ <ul id="nav">
+ <li><a href="#">Dashboard</a></li>
+ <li><a href="#">analytics</a></li>
+ <li><a href="#">Options</a></li>
+ <li><a href="#">Dashboard</a></li> 
+ <li><a href="#">Menu</a></li>
+ <li><a href="#">Options</a></li>
+ <li><a href="#">Dashboard</a></li>     
+ </div>
+<div class="content"> Analytics </div>
+ </div>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,32 @@
+
+<html>
+<head>
+<title> Sign up</title>	
+<link rel="stylesheet" type="text/css" href="styles/signup.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div id="reg">
+<form>	
+	
+<div class="spacing">First Name:<input type="text" class="form-control"id="fn" placeholder="Enter First Name"></div>
+<div class="spacing">Last Name:<input type="text" class="form-control"id="fn" placeholder="Enter Last Name"></div>
+<div class="spacing"> Email:<input type="email" class="form-control"id="email" placeholder="Enter Email"></div>
+<div class="spacing">Password:<input type="password" class="form-control"id="pwd" placeholder="Enter Password"></div>
+<div class="spacing">Confirm-Password:<input type="password" class="form-control"id="confirm pwd" placeholder="Re-Enter Password"></div>
+<div class="spacing"><button type="submit" class="btn btn-default">Sign Up</button></div>
+			
+</form>	
+	
+	
+</div>		
+	
+	
+	
+</body>
+
+
+
+</html>
